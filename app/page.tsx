ddcd9a584728c7a18606b155935dccfc2f07c73d
Siddhant Pandey <pandeysiddhant0922@gmail.com>
2026-04-27 07:40:06 +0545

    add the images tab

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,36 @@
+import ImagesTab from "@/components/images-tab";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 bg-white min-h-screen">
       <main className="flex-1 ">
-        <section className="container mx-auto px-4 py-32">
+        <section className="container mx-auto px-4 py-32 ">
           <div className="text-black text-center">
-            <h1 className="text-6xl font-semibold mb-6">A better way to track the job application.</h1>
-            <p className="text-muted-foreground mb-10 text-sm">Capture & Manage the job application in one place.</p>
+            <h1 className="text-6xl font-semibold lg:font-bold mb-6 max-w-6xl">
+              A better way to track the job application.
+            </h1>
+            <p className="text-muted-foreground mb-10 text-sm">
+              Capture & Manage the job application in one place.
+            </p>
           </div>
           <div className=" flex flex-col gap-2 items-center">
-            <Button>Get Started <ArrowRight className="ml-2"/></Button>
-            <p className="text-muted-foreground text-sm">Free for all. No Money required.</p>
+            <Link href="/sign-up">
+              <Button>
+                Get Started <ArrowRight className="ml-2" />
+              </Button>
+            </Link>
+            <p className="text-muted-foreground text-sm">
+              Free for all. No Money required.
+            </p>
           </div>
         </section>
+
+        {/* image section */}
+        <ImagesTab/>
+
       </main>
     </div>
   );
---
+++ components/images-tab.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function ImagesTab() {
+  const [activeTab, setActiveTab] = useState("organise");
+
+  return (
+    <section className="border-t border-gray-200 py-12">
+      <div className="container mx-auto px-4 max-w-6xl">
+        <h1 className="text-center mb-6 font-bold text-3xl text-gray-700">
+          All your applications. One dashboard.
+        </h1>
+        <div className="flex justify-center gap-4 mb-4 max-w-5xl">
+          <Button
+            onClick={() => setActiveTab("organise")}
+            className={`rounded-sm transition-color hover:text-white ${activeTab === "organise" ? "bg-primary text-white" : "bg-accent "}`}
+          >
+            Organize
+          </Button>
+          <Button
+            onClick={() => setActiveTab("hired")}
+            className={`rounded-sm transition-color hover:text-white ${activeTab === "hired" ? "bg-primary text-white" : "bg-accent"}`}
+          >
+            Hired
+          </Button>
+          <Button
+            onClick={() => setActiveTab("board")}
+            className={`rounded-sm transition-color hover:text-white ${activeTab === "board" ? "bg-primary text-white" : "bg-accent"}`}
+          >
+            Board
+          </Button>
+        </div>
+        <div className="mx-auto border border-gray-200">
+          {activeTab === "organise" && (
+            <Image
+              alt="Organized"
+              src="/hero-images/organize.jpg"
+              width={1200}
+              height={800}
+              className="rounded-sm border-accent border"
+            />
+          )}
+          {activeTab === "hired" && (
+            <Image
+              alt="Hired"
+              src="/hero-images/hired.png"
+              width={1200}
+              height={800}
+              className="rounded-sm border-accent border"
+            />
+          )}
+
+          {activeTab === "board" && (
+            <Image
+              alt="Board"
+              src="/hero-images/boards.jpg"
+              width={1200}
+              height={800}
+              className="rounded-sm border-accent border"
+            />
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
