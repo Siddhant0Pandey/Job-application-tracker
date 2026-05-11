269f6af3bc616bda61754227232dcfaf2aaebc71
Siddhant Pandey <pandeysiddhant0922@gmail.com>
2026-05-11 18:34:15 +0545

    auth setup

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+export default function DashboardPage() {
+    return <div className="container mx-auto px-4 py-12 max-w-6xl">
+        <h1 className="text-3xl font-bold mb-6 text-gray-700">Welcome to the Dashboard</h1>
+        
+    </div>
+}
